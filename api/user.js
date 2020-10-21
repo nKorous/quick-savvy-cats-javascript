532b544f59c53a307eadb66db6dcec586e91615f
Nate Korous <development.nk@gmail.com>
2020-10-20 22:32:08 -0600

    added transactions

--- api/user.js
+++ api/user.js
@@ -14,16 +14,6 @@ router.get('/login/:id/:pw', async (req, res) => {
     res.send(findUser)
 })
 
-router.get('/:id', async (req, res) => {
-    const id = decodeURI(req.params.id)
-    try {
-        let returnUser = db.get('users').find({ email: id})
-        res.status(200).send(returnUser)
-    } catch (err) {
-        res.status(500).send({Error: err})
-    }
-})
-
 router.post('/update/:id', async (req, res) => {
 
 })
